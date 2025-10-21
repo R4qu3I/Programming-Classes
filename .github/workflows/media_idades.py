a8e1ae836b26bb73388568df784d3d97a62f4660
Raquel Paiva <145876298+R4qu3I@users.noreply.github.com>
2025-10-21 08:40:40 -0300

    Create media_idades.py

---
+++ .github/workflows/media_idades.py
@@ -0,0 +1,10 @@
+idade1=int(input("Digite a 1° idade: "))
+idade2=int(input("Digite a 2° idade: "))
+idade3=int(input("Digite a 3° idade: "))
+idade4=int(input("Digite a 4° idade: "))
+
+
+media=(idade1+idade2+idade3+idade4)/4
+
+
+print(f"A médida de idade das crianças é de {media}")
